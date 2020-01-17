c5f1b215bd51ca4529e54dc70be98bb232e9e5d0
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2020-01-17 15:17:05 +0000

    feat:header

--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,72 @@
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import Media from './media'
+import Img from 'gatsby-image'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  const avatar = useStaticQuery(graphql`
+    query imageQuery {
+      file(relativePath: { eq: "daniela.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 256) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        display: 'flex',
-        justifyContent: 'space-between'
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+          display: 'flex',
+          justifyContent: 'flex-start'
+        }}
+      >
+        <Img
           style={{
-            color: `#FB4485`,
-            textDecoration: `none`,
+            border: 'solid',
+            borderRadius: '50%',
+            borderColor: 'transparent',
+            borderWidth: '0.5rem',
+            width: '6rem'
           }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-
-      <div style={{ 
-        alignSelf: 'flex-end'
-      }}>
-        <Media />
+          fluid={avatar.file.childImageSharp.fluid}
+          alt="Daniela's personal picture"
+        />
+        <div style={{
+            alignSelf: 'center'
+          }}>
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: `#FB4485`,
+                textDecoration: `none`,
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+          <div style={{
+            alignSelf: 'flex-end'
+          }}>
+            <Media />
+          </div>
+        </div>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
