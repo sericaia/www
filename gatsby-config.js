1bf8cede0b394128ba4deed6d62836a68ad9c1b4
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2020-01-17 13:32:41 +0000

    fix: config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Daniela Matos de Carvalho`,
+    description: `Daniela Matos de Carvalho personal website - blog and contacts`,
+    author: `@sericaia`,
+    siteUrl: `https://www.sericaia.me`,
+    links: {
+      email: "danielabmcarvalho@gmail.com",
+      github: "https://github.com/sericaia",
+      linkedin: "https://www.linkedin.com/in/danielabmcarvalho",
+      twitter: "https://twitter.com/sericaia",
+    },
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +18,21 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images\/.*\.svg/,
+        }
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,7 +46,16 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/shortcake.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [".md", ".mdx"],
+        defaultLayouts: {
+          blog: require.resolve("./src/components/layout.js"),
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
