eb40b558f1a09d6c46aac6d0464ea05ac960359f
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2020-01-20 09:29:51 +0000

    fix: articles order

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,22 @@ import React from "react"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 import Articles from "../components/articles"
+import Navigation from "../components/navigation"
 
 const IndexPage = ({ data }) => {
   const { posts } = data
-
-  const postsDesc = posts.nodes.reverse()
   return (
     <Layout>
       <SEO title="Home" />
-      <Articles articles={postsDesc} />
+      <Navigation />
+      <Articles articles={posts.nodes} />
     </Layout>
   )
 }
 
 export const pageQuery = graphql`
   query allPostsQuery {
-    posts: allMdx(sort: { fields: frontmatter___date }) {
+    posts: allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       totalCount
       nodes {
         id
