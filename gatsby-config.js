9550a2fd20880acd2092b639ea3cf6898d3a6277
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2020-01-31 09:23:17 +0000

    feat: offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -138,8 +138,6 @@ module.exports = {
         },
       },
     },
+    `gatsby-plugin-offline`,
   ],
-  // this (optional) plugin enables Progressive Web App + Offline functionality
-  // To learn more, visit: https://gatsby.dev/offline
-  // `gatsby-plugin-offline`,
 }
